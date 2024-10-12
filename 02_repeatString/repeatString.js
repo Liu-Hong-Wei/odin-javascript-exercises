9bc169c6dd40a46350ef593a0b6e2616c5168dc4
Homey Lau <lhw1953927853@gmail.com>
2024-10-12 15:32:36 +0800

    finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    if (times < 0) {
+        console.error("Invalid repeat times!");
+    }
+    else if (times == 0) {
+        return "";
+    }
 
+    for (; times > 0; --times) {
+        str += str;
+    }
+    return str;
 };
 
 // Do not edit below this line

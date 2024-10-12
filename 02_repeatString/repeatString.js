0eea59a2516c48defd4682e9847f5338b8255174
homeylau <lhw1953927853@gmail.com>
2024-10-12 15:49:37 +0800

    finish 02 03 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,16 @@
 const repeatString = function(str, times) {
     if (times < 0) {
-        console.error("Invalid repeat times!");
+        return "ERROR";
     }
     else if (times == 0) {
         return "";
     }
 
+    res = ""
     for (; times > 0; --times) {
-        str += str;
+        res += str;
     }
-    return str;
+    return res;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    res = "";
+    for (const c of str) {
+        res = c + res;
+    }
+    return res;
 };
 
 // Do not edit below this line

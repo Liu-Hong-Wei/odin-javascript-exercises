2043c84ead8d0c74a2f33c95cb0424719e1de1d3
homeylau <lhw1953927853@gmail.com>
2024-10-13 11:49:37 +0800

    finish 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+  if (
+    typeof first != "number" ||
+    typeof second != "number" ||
+    first < 0 ||
+    second < 0 ||
+    first % 1 !== 0 ||
+    second % 1 !== 0
+  ) {
+    return "ERROR";
+  }
+  minOne = first > second ? second : first;
+  maxOne = first > second ? first : second;
+  let res = 0;
+  for (let i = minOne; i <= maxOne; ++i) {
+    res += i;
+  }
+  return res;
 };
 
 // Do not edit below this line

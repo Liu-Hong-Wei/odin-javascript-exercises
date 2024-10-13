d1da4aa17bb3c9905e18d3487ce8689d98810de7
homeylau <lhw1953927853@gmail.com>
2024-10-13 11:37:16 +0800

    finish 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
+
+// const removeFromArray = function(array, ...args) {
+//     return array.filter(val => !args.includes(val));
+// }
+
 const removeFromArray = function() {
+    let res = []
+    let hasSkip = false;
+    for (let i = 0; i < arguments[0].length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (arguments[0][i] === arguments[j]) {
+                hasSkip = true;
+                break;
+            }
+        }
+        if (!hasSkip) {
+            res.push(arguments[0][i]);
+        }
+        hasSkip = false;
+    }
+    return res;
 };
 
 // Do not edit below this line

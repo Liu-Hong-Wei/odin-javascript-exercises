782e4ac45ce7a7a265e2668df981c9d0a97f6280
homeylau <lhw1953927853@gmail.com>
2024-10-13 12:11:04 +0800

    finish 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (n) {
+  let res = ((n - 32) * 5) / 9
+  return res % 1 === 0 ? res : Math.round(res * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (n) {
+  let res = (n * 9) / 5 + 32;
+  return res % 1 === 0 ? res : Math.round(res * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

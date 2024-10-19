9721d641687e28cb24a8446c91e055e88a38442b
homeylau <lhw1953927853@gmail.com>
2024-10-19 20:06:36 +0800

    finish  10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  let a = 1;
+  let b = 1;
+  if (n == 0) a = 0;
+  if (isNaN(n) || n < 0) a = "OOPS"
+  while (n > 2) {
+    let t = a;
+    a = a + b;
+    b = t;
+    n--;
+  }
+  return a;
 };
 
 // Do not edit below this line

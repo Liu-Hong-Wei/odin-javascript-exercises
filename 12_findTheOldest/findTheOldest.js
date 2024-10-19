6783e1941e4aebe1bf0c3761319dbc2c8a777663
homeylau <lhw1953927853@gmail.com>
2024-10-19 20:24:13 +0800

    finish 12 exercise and learnt about using (new Date().getFullyear()) and ??=

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.map((item) => {
+    if (!item.yearOfDeath) {
+      item.yearOfDeath = 2024;
+    }
+    return {
+      name: item.name,
+      yearOfBirth: item.yearOfBirth,
+      yearOfDeath: item.yearOfDeath,
+    };
+  });
+  return people
+    .map((item) => ({
+      year: item.yearOfDeath - item.yearOfBirth,
+      name: item.name,
+    }))
+    .sort((a, b) => b.year - a.year)[0];
 };
 
 // Do not edit below this line
